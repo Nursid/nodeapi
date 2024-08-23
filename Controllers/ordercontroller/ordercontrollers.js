4741f972411eb26f1e295570dd98b89a51590f43
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2024-08-24 00:15:26 +0530

    updated

--- Controllers/ordercontroller/ordercontrollers.js
+++ Controllers/ordercontroller/ordercontrollers.js
@@ -374,7 +374,8 @@ const GetOrderByID = async (req, res) => {
 				attributes: ['name', 'email', 'mobileno'],
 				include: {
 					model: CustomerModel,
-					attributes: ['age', 'address', 'member_id'],
+					attributes: [ 'gender', 'age', 'address', 'land_mark', 'location', 'tel_no', 'office_no', 'alternate_no', 'aadhar_no', 'occupation', 'designation', 'own_house', 'dob', 'doa', 'spouse_name', 'spouse_name1', 'spouse_dob1', 'spouse_name2', 'spouse_dob2', 'spouse_dob', 'image','service', 'service1', 'service2', 'service3', 'service4', 'service5', 'username', 'reference', 'familyMember', 'membership', 'is_approved', 'member_id', 'is_block', 'todate', 'validtodate', 'createdAt',
+					],
 				}
 			},
 			order: [['id', 'DESC']],
