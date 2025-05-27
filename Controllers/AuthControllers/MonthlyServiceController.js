114c77be2da5f5fbc66818463cfa61ad2e8da7ea
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2025-05-27 23:40:30 +0530

    updated

--- Controllers/AuthControllers/MonthlyServiceController.js
+++ Controllers/AuthControllers/MonthlyServiceController.js
@@ -813,15 +813,10 @@ const MonthlyServiceCheckOut = async (req, res) => {
             // Generate time slots
             // const formattedTime = moment(isService.checkintime, "MM/DD/YYYY, h:mm A").format("MM/DD/YYYY, hh:mm A");
 
-            // const formattedTime = moment(isService.checkintime, "DD/MM/YYYY, h:mm A").format("MM/DD/YYYY, hh:mm A");
-            const formattedTime = "2025-05-27, 07:00 AM";
-
-            console.log(formattedTime);
+            const formattedTime = moment(isService.checkintime, "DD/MM/YYYY, h:mm A").format("MM/DD/YYYY, hh:mm A");
 
             // const currentDateTime = moment().tz("Asia/Kolkata").format("DD/MM/YYYY, hh:mm A");
-            // const currentDateTime = moment2().tz("Asia/Kolkata").format("MM/DD/YYYY, hh:mm A");
-            const currentDateTime = "2025-05-27, 08:00 AM";
-
+            const currentDateTime = moment2().tz("Asia/Kolkata").format("MM/DD/YYYY, hh:mm A");
 
             const formattedOrders = getTimeSlots(formattedTime, currentDateTime);
             const slots = convertSlotsTo12Hour(formattedOrders);
